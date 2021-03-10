94c64789e262049a062f54ea96ab3896e9959388
raquel-vazquez <lvazquez@novasolutionsystems.com>
2021-03-09 19:11:02 -0600

    feat: add React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import { Fragment } from 'react';
 import Header from './components/Header';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Productos from './components/Productos';
+import NuevoProducto from './components/NuevoProducto';
+import EditarProducto from'./components/EditarProducto';
 
 function App() {
   return (
-    <Fragment>
+    <Router>
       <Header />
-    </Fragment>
+
+      <div className="container mt-5">
+        <Switch>
+          <Route  exact path="/" component={Productos}/>
+          <Route exact path="/productos/nuevo" component={NuevoProducto}/>
+          <Route exact path="/productos/editar/:id" component={EditarProducto}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/NuevoProducto.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NuevoProducto = () => {
+    return ( 
+        <h1>desde nuevo producto</h1>
+     );
+}
+ 
+export default NuevoProducto;
